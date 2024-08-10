30be6ae1f1835983373810c2bde81cff946b1fa9
Abhishek <101599136+Abhishek-guptaaa@users.noreply.github.com>
2024-08-10 19:37:32 +0530

    folder structure done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+import setuptools
+setuptools.setup(
+    name="Text Summarization",
+    version='0.0.1',
+    author="Abhishek",
+    author_email="abhishek18895gupta@gmail.com",
+    packages=find_packages(),
+)
